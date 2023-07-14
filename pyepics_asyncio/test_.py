5e90de2903ef49c366ca58b48337c9c6d8fe39b9
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-07-14 14:28:06 +0700

    Test array PVs

--- pyepics_asyncio/test_.py
+++ pyepics_asyncio/test_.py
@@ -45,3 +45,44 @@ async def test_scalar_monitor() -> None:
         await output.put(value)
 
     await task
+
+
+@pytest.mark.asyncio
+async def test_array() -> None:
+    rng = random.Random(SEED)
+
+    output = await Pv.connect("ca:test:aao")
+    input = await Pv.connect("ca:test:aai")
+
+    max_len = output.nelm
+    for i in range(0, ATTEMPTS):
+        value = [rng.randint(-(2**31), 2**31 - 1) for i in range(0, rng.randint(2, max_len))]
+        await output.put(value)
+        assert list(await input.get()) == value
+
+
+@pytest.mark.asyncio
+async def test_array_monitor() -> None:
+    output = await Pv.connect("ca:test:aao")
+    max_len = output.nelm
+
+    rng = random.Random(SEED)
+    values = [[rng.randint(-(2**31), 2**31 - 1) for j in range(0, rng.randint(2, max_len))] for i in range(0, ATTEMPTS)]
+
+    async def monitor(pv: PvMonitor) -> None:
+        i = 0
+        async for value in pv:
+            assert list(value) == values[i]
+            i += 1
+            if i >= len(values):
+                break
+
+    await output.put(values[0])
+
+    input = await PvMonitor.connect("ca:test:aai")
+    task = asyncio.get_running_loop().create_task(monitor(input))
+
+    for value in values[1:]:
+        await output.put(value)
+
+    await task
