50b17394a0709c06535214e8747fb416f88db08c
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-07-14 14:13:35 +0700

    Test randomly generated sequence

--- pyepics_asyncio/test_.py
+++ pyepics_asyncio/test_.py
@@ -1,38 +1,47 @@
 from __future__ import annotations
 
-import math
+import random
 import asyncio
 from pyepics_asyncio import Pv, PvMonitor
 import pytest
 
+ATTEMPTS = 256
+SEED = 0xDEADBEFF
+
 
 @pytest.mark.asyncio
 async def test_scalar() -> None:
+    rng = random.Random(SEED)
+
     output = await Pv.connect("ca:test:ao")
     input = await Pv.connect("ca:test:ai")
 
-    await output.put(math.e)
-    assert await input.get() == math.e
-
-    await output.put(math.pi)
-    assert await input.get() == math.pi
+    for i in range(0, ATTEMPTS):
+        value = rng.gauss(0.0, 1.0)
+        await output.put(value)
+        assert await input.get() == value
 
 
 @pytest.mark.asyncio
 async def test_scalar_monitor() -> None:
+    rng = random.Random(SEED)
+    values = [rng.gauss(0.0, 1.0) for i in range(0, ATTEMPTS)]
+
     async def monitor(pv: PvMonitor) -> None:
-        mon = pv.__aiter__()
-        assert await mon.__anext__() == 0.0
-        assert await mon.__anext__() == math.e
-        assert await mon.__anext__() == math.pi
+        i = 0
+        async for value in pv:
+            assert value == values[i]
+            i += 1
+            if i >= len(values):
+                break
 
     output = await Pv.connect("ca:test:ao")
-    await output.put(0.0)
+    await output.put(values[0])
 
     input = await PvMonitor.connect("ca:test:ai")
     task = asyncio.get_running_loop().create_task(monitor(input))
 
-    await output.put(math.e)
-    await output.put(math.pi)
+    for value in values[1:]:
+        await output.put(value)
 
     await task
